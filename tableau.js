03bdb2f1bba5d31ce56b145cf51b829a53d49f2c
Ali Shujjat <48991374+AliShujjat@users.noreply.github.com>
2020-10-31 01:30:45 +0500

    Update tableau.js

--- tableau.js
+++ tableau.js
@@ -1,11 +1,11 @@
 function initializeViz() {
 var placeholderDiv = document.getElementById("tableauViz");
-var url = "http://public.tableau.com/views/WorldIndicators/GDPpercapita";
+var url = "https://public.tableau.com/shared/RRC4FYXYQ?:display_count=y&:origin=viz_share_link";
 var options = {
  width: '600px',
  height: '600px',
  hideTabs: true,
  hideToolbar: true,
  };
-viz = new tableau.Viz(placeholderDiv, url, options);
+var viz = new tableau.Viz(placeholderDiv, url, options);
 }
