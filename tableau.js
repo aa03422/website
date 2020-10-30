dd5d76bca184bcd95ee032f2f617a279212ccd3f
Ali Shujjat <48991374+AliShujjat@users.noreply.github.com>
2020-10-31 01:34:54 +0500

    Update tableau.js

--- tableau.js
+++ tableau.js
@@ -1,11 +1,5 @@
 function initializeViz() {
 var placeholderDiv = document.getElementById("tableauViz");
 var url = "https://public.tableau.com/shared/RRC4FYXYQ?:display_count=y&:origin=viz_share_link";
-var options = {
- width: '600px',
- height: '600px',
- hideTabs: true,
- hideToolbar: true,
- };
-var viz = new tableau.Viz(placeholderDiv, url, options);
+var viz = new tableau.Viz(placeholderDiv, url);
 }
