b6560cd2e0a7a8a7dd3affb291bd4fbbe3178f3f
Ali <as03856@st.habib.edu.pk>
2020-10-31 02:13:40 +0500

    Using API now

--- tableau.js
+++ tableau.js
@@ -1,5 +1,13 @@
-function initializeViz() {
-var placeholderDiv = document.getElementById("tableauViz");
-var url = "https://public.tableau.com/views/PakistanAvg_ChildAge/Sheet1?:language=en-GB&:display_count=y&:origin=viz_share_link";
-viz = new tableau.Viz(placeholderDiv, url);
-}
+var viz;
+
+window.onload = function() {
+    var vizDiv = document.getElementById('myViz')
+    var vizURL = "https://public.tableau.com/views/PakistanAvg_ChildAge/Sheet1?:language=en-GB&:display_count=y&:origin=viz_share_link";
+    var options = {
+        width: '600px',
+        height: '540px'
+    };
+    viz = new tableau.Viz(vizDiv, vizURL, options);
+};
+
+var tableau;
