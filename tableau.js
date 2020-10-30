e2ceb37e4134b0b1885cdae0878d13439e13ae76
Ali Shujjat <48991374+AliShujjat@users.noreply.github.com>
2020-10-31 01:42:48 +0500

    Update tableau.js

--- tableau.js
+++ tableau.js
@@ -1,5 +1,5 @@
 function initializeViz() {
 var placeholderDiv = document.getElementById("tableauViz");
-var url = "http://public.tableau.com/views/WorldIndicators/GDPpercapita";
+var url = "https://public.tableau.com/views/PakistanAvg_ChildAge/Sheet1?:language=en-GB&:display_count=y&:origin=viz_share_link";
 viz = new tableau.Viz(placeholderDiv, url);
 }
