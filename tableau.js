f73d2c8e93b3b6d0a18e2f17087fa96c27f95492
Ali <as03856@st.habib.edu.pk>
2020-10-31 12:59:07 +0500

    Added Habib logo and About Us page

--- tableau.js
+++ tableau.js
@@ -1,13 +1,33 @@
+// Initialize the viz variable
 var viz;
 
-window.onload = function() {
-    var vizDiv = document.getElementById('myViz')
-    var vizURL = "https://public.tableau.com/views/PakistanAvg_ChildAge/Sheet1?:language=en-GB&:display_count=y&:origin=viz_share_link";
-    var options = {
-        width: '600px',
-        height: '540px'
-    };
-    viz = new tableau.Viz(vizDiv, vizURL, options);
+window.onload= function() {
+// When the webpage has loaded, load the viz
+	var vizDiv = document.getElementById('myViz');
+	var vizURL = 'https://public.tableau.com/views/PakistanAvg_ChildAge/Sheet1?:language=en-GB&:display_count=y&publish=yes&:origin=viz_share_link';
+	var options = {
+		width: '600px',
+		height: '540px',
+		hideToolbar: true,
+		hideTabs: true
+	};
+	viz = new tableau.Viz(vizDiv, vizURL, options);
 };
 
-var tableau;
+// Switch the viz to the sheet specified
+function switchView(sheetName) {
+	var workbook = viz.getWorkbook();
+	workbook.activateSheetAsync(sheetName);
+}
+
+// Filter the specified dimension to the specified value(s)
+function show(filterName, values) {
+	var sheet = viz.getWorkbook().getActiveSheet();
+    sheet.applyFilterAsync(filterName, values, tableau.FilterUpdateType.REPLACE);
+}
+
+// Select the marks that have the specified value(s) for the specified dimension
+function selectMarks(filterName, values) {
+	var sheet = viz.getWorkbook().getActiveSheet();
+    sheet.selectMarksAsync(filterName, values, tableau.FilterUpdateType.REPLACE);
+}
