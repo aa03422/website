d933d41f9d4b9b359ad5c3d2782288b9e0559a62
Ali Shujjat <48991374+AliShujjat@users.noreply.github.com>
2020-11-28 21:27:52 +0500

    Update tableau.js

--- tableau.js
+++ tableau.js
@@ -2,7 +2,7 @@ var viz;
 
 window.onload= function() {
 	var vizDiv = document.getElementById('myViz');
-	var vizURL = 'https://public.tableau.com/views/PakistanAvg_ChildAge/Sheet1?:language=en-GB&:display_count=y&publish=yes&:origin=viz_share_link';
+	var vizURL = 'https://public.tableau.com/views/DSProject_16064140022170/ReasonsDropout?:language=en-GB&:display_count=y&publish=yes&:origin=viz_share_link';
 	var options = {
 		width: '600px',
 		height: '540px',
