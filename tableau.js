f51c3d25723dc98f541756624925022f77dc1c97
Ali Shujjat <48991374+AliShujjat@users.noreply.github.com>
2020-11-28 21:33:00 +0500

    Update tableau.js

--- tableau.js
+++ tableau.js
@@ -4,7 +4,7 @@ window.onload= function() {
 	var vizDiv = document.getElementById('myViz');
 	var vizURL = 'https://public.tableau.com/views/DSProject_16064140022170/ReasonsDropout?:language=en-GB&:display_count=y&publish=yes&:origin=viz_share_link';
 	var options = {
-		width: '600px',
+		width: '1050px',
 		height: '540px',
 		hideToolbar: true,
 		hideTabs: true
